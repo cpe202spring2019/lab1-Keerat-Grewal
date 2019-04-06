ca622e4a3040391225f1085ddc3a562bb298d2d9
Keerat Grewal <kgrewa03@calpoly.edu>
2019-04-06 00:29:10 -0700

    second commit

--- lab1.py
+++ lab1.py
@@ -13,12 +13,18 @@ def max_list_iter(int_list):  # must use iteration not recursion
     return int_max
     pass
 
+
 def reverse_rec(int_list):   # must use recursion
     """recursively reverses a list of numbers and returns the reversed list
     If list is None, raises ValueError"""
+    count = len(int_list) - 1
     if int_list is None:
         raise ValueError
+    if count == 0:
+        return []
+    return reverse_rec(int_list[count - 1]) + int_list[count]
     pass
+
 
 def bin_search(target, low, high, int_list):  # must use recursion
     """searches for target in int_list[low..high] and returns index if found
